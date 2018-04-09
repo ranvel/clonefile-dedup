b28187895486c1b0126de4c9444192a333b34fc6
ranvel <ranvel@ranvel.com>
2018-04-09 18:15:50 -0400

    sqlite fix
    Now properly creates the table in accordance with the latest documentation at: https://docs.python.org/3/library/sqlite3.html

--- clonefile-index.py
+++ clonefile-index.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import os, subprocess, sqlite3
 from os import listdir
 from os.path import isfile, join
 
 conn = sqlite3.connect('index.sqlite')
+c = conn.cursor()
+c.execute('''CREATE TABLE files (file, chksum)''')
 
 def processFile(filelink):
 	# Don't worry about tiny files:
@@ -11,7 +13,7 @@ def processFile(filelink):
 		print(filelink)
 		shahash = getSHA256(filelink).split()[0]
 		print(shahash.decode("utf-8"))
-		conn.execute("INSERT INTO files (file, chksum) VALUES ('"+filelink.replace('\'', "\'\'") +"', '"+shahash.decode("utf-8")+"');")
+		c.execute("INSERT INTO files (file, chksum) VALUES ('"+filelink.replace('\'', "\'\'") +"', '"+shahash.decode("utf-8")+"');")
 		conn.commit()
 		print("\n")
 
